b61ae131101f4af06e495cc07134d776b89ebbeb
RawanAkAzz <rawan.akram27@yahoo.com>
2019-06-15 18:06:05 +0300

    Update Component

--- src/Components/AboutComp/About.jsx
+++ src/Components/AboutComp/About.jsx
@@ -1,14 +1,15 @@
 import React, {Component} from '../../../node_modules/react'
-class About extends Component{
-    render(){
-         const divStyle = {
+ const divStyle = {
                 margin: '40px',
                background:'pink',
                 border: '5px solid pink'
               };
+              class About extends Component{
+    render(){
+        
         return(
            
-       <div id="e">  <div style={divStyle}/>
+       <div className="p-3 mb-2 bg-warning text-dark" id="e">  <div style={divStyle}/>
         <div  className="br2 ba  mv4 w-100 w-50-m w-25-l mw5 center" style={{width: '300px', height: '50px',padding:'10px'}} >
 
         <center>
