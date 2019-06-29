6c24bdf39f21a9f2c48511d50cdd375439af1543
RawanAkAzz <rawan.akram27@yahoo.com>
2019-06-29 15:46:44 +0300

    initial commit

--- src/Components/AboutComp/About.jsx
+++ src/Components/AboutComp/About.jsx
@@ -1,21 +1,36 @@
 import React, {Component} from '../../../node_modules/react'
  const divStyle = {
                 margin: '40px',
-               background:'pink',
-                border: '5px solid pink'
+                border: '5px solid'
               };
               class About extends Component{
     render(){
         
         return(
            
-       <div className="p-3 mb-2 bg-warning text-dark" id="e">  <div style={divStyle}/>
+       <div className="p-3 mb-2" id="e">  <div style={divStyle}/>
         <div  className="br2 ba  mv4 w-100 w-50-m w-25-l mw5 center" style={{width: '300px', height: '50px',padding:'10px'}} >
 
         <center>
            About App 
            </center> 
-            </div></div>
+          
+            </div> 
+            <p>
+           Hospital information systems provide a common source of information about a patient's health history. The system has to keep data in a secure place and controls who can reach the data in certain circumstances. These systems enhance the ability of health care professionals to coordinate care by providing a patient's health information and visit history at the place and time that it is needed. Patient's laboratory test information also includes visual results such as X-ray, which may be reachable by professionals. HIS provide internal and external communication among health care providers. Portable devices such as smartphones and tablet computers may be used at the bedside.
+           </p>
+           <p className="mb-4">
+           Potential benefits of hospital information systems include: <br/>
+</p>
+<p>
+  <ol>
+* Efficient and accurate administration of finance, diet of patient, engineering, and distribution of medical aid. It helps to view a broad picture of hospital growth <br/>
+* Improved monitoring of drug usage, and study of effectiveness. This leads to the reduction of adverse drug interactions while promoting more appropriate pharmaceutical utilization. <br/>
+* Enhances information integrity, reduces transcription errors, and reduces duplication of information entries. <br/>
+* Hospital software is easy to use and eliminates error caused by handwriting. New technology computer systems give perfect performance to pull up information from server or cloud servers.
+</ol>
+           </p>
+           </div>
           )
     }
 }
--- src/Components/Image/Image.jsx
+++ src/Components/Image/Image.jsx
@@ -2,14 +2,16 @@ import React, { Component } from "../../../node_modules/react";
 
 class Image extends Component {
   render() {
+    const styleii={
+      fontSize:"1rem",
+      marginButtom:"8rem"
+    }
     return(
       <article class="athelas">
         <div class="vh-100 dt w-100 cover  " style={{background:'url(https://www.popsci.com/sites/popsci.com/files/images/2016/12/computer-1149148_1920.jpg) no-repeat center '}}>
           <div class="dtc v-mid">     
-            <div class="f1 center  w-20 tc pv5 bg-black-20 " style={{color:'#FFF'}}>
-              <p>WRITE HERE</p>
-              <br/>
-              <p>fddfdfdfjdfjj</p>
+            <div class="f1 f-1 mt-19 center mb-5 w-20 tc pv5 bg-black-20 " style={{color:'#FFF'}} style= {styleii}>
+              <p className="f1 center mt-7 mb-2 wh-100" >Good health is not something we can buy. However, it can be an extremely valuable savings account.</p>
             </div>
           </div>
         </div>   
