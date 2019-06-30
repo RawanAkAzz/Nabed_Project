39429f161ffe62364c8297b8dc2f44c3967774d3
RawanAkAzz <rawan.akram27@yahoo.com>
2019-06-30 11:37:07 +0300

    dep

--- Server/Server.js
+++ Server/Server.js
@@ -4,19 +4,14 @@ const app = express();
 var cors = require("cors");
 var firebase = require("firebase/app");
 const bodyParser = require("body-parser");
-app.use('/static', express.static(path.join(__dirname, '../build/static')));
+// app.use('/static', express.static(path.join(__dirname, '../build/static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*"); 
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	 next();
-  });
 
-app.get("/*", (req, res) => {
-	res.sendFile('index.html', {root: path.join(__dirname, '../build/')});
-})
+// app.get("/*", (req, res) => {
+// 	res.sendFile('index.html', {root: path.join(__dirname, '../build/')});
+// })
 
 // app.post('/ProfilePage',(req,res) => {
 //    var image = req.body.image;
